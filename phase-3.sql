b6d39cc6094aeb26df23d5a7422a85571e2a34a6
Matt <matthewdavid99@comcast.net>
2023-01-13 13:33:00 -0500

    fixed files: phase-3.sql

--- phase-3.sql
+++ phase-3.sql
@@ -3,11 +3,11 @@
 
 -- -- -- (Write a query to find all the cats' names for the owner with the first name of "Hermione". You must do this in one query. If you have trouble doing it in one query, try doing it in multiple and then refactor to just one query.)
 
-SELECT cats.name
-FROM cats
-JOIN cat_owners ON cat_owners.cat_id = cats.id
-JOIN owners ON cat_owners.owner_id = owners.id
-WHERE owners.first_name = 'Hermione';
+-- SELECT cats.name
+-- FROM cats
+-- JOIN cat_owners ON cat_owners.cat_id = cats.id
+-- JOIN owners ON cat_owners.owner_id = owners.id
+-- WHERE owners.first_name = 'Hermione';
 
 -- (REFACTOR⬇️⬇️⬇️⬇️⬇️)----------------------------------------------------
 
